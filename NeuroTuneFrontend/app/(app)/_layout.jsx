ad93addad9d52c5126189ca0391013170d1578d7
WillD55 <wdrake@wesleyan.edu>
2025-04-16 01:13:44 -0400

    Update _layout.jsx

--- NeuroTuneFrontend/app/(app)/_layout.jsx
+++ NeuroTuneFrontend/app/(app)/_layout.jsx
@@ -7,6 +7,7 @@ export default function appLayout() {
       <Tabs.Screen name="list" options={{ title: "List" }} />
       <Tabs.Screen name="update" options={{ title: "Update" }} />
       <Tabs.Screen name="delete" options={{ title: "Delete" }} />
+      <Tabs.Screen name="logout" options={{ title: "Logout" }} />
     </Tabs>
   );
 }
