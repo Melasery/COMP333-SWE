743bdb9231bd7a14db86029b6ef961ba0ace5ebf
felixberliner1 <61637800+felixberliner1@users.noreply.github.com>
2025-04-25 21:43:38 -0400

    Update create.jsx
    (Create is even more of a mess now - sorry)

--- NeuroTuneFrontend/app/(app)/create.jsx
+++ NeuroTuneFrontend/app/(app)/create.jsx
@@ -8,7 +8,10 @@ export default function CreateScreen() {
   const [song, setSong] = useState("");
   const [artist, setArtist] = useState("");
   const [rating, setRating] = useState("");
+  const [otherData, setOtherData] = useState([]);
+  const [currSim, setCurrSim] = useState(0);
   const { user } = useAuth();
+
 
   //Sends create request to REST api
   const createRating = async () => {
@@ -49,7 +52,6 @@ export default function CreateScreen() {
                     const lyrIsJson = lyrContentType && lyrContentType.includes("application/json");
               
                     const lyrText = lyrIsJson ? await lyrResponse.json() : await lyrResponse.text();
-                    console.log("Raw lyric response:", lyrText);
                     console.log("Lyrics found");
                     if (lyrIsJson && lyrText.lyrics !== "") {
                       const lyrics = lyrText.lyrics;
@@ -78,6 +80,66 @@ export default function CreateScreen() {
                           if (lprIsJson && lprText.message === "Data Created Sucessfully")
                           console.log(lyrText.message);
                           console.log("Lyrics uploaded successfully");
+
+                          try {
+                            const odResponse = await fetch(`${BASE_URL}/data/listExclusion`, {
+                              method: "POST",
+                              headers: { "Content-Type": "application/json" },
+                              body: JSON.stringify({ 
+                                "song": song, 
+                                "artist": artist
+                              }),
+                            });
+                            const odText = await odResponse.json();
+                            setOtherData(odText);
+                            console.log(odText);
+                            console.log(otherData);
+                            for (let i = 0; i < odText.length; i++) {
+                              try {
+                                const simResponse = await fetch("https://api.api-ninjas.com/v1/textsimilarity", {
+                                  method: "POST",
+                                  headers: {
+                                    "X-Api-Key": "gcSOweST4vXedgtN26Z3xg==Guqol9FxgYHuKUBT"
+                                  },
+                                  //Uses the lyrics for both songs, if we implement a stripped version of the lyrics data you would swap those in here
+                                  body: JSON.stringify({
+                                    "text_1": lyrics,
+                                    "text_2": odText[i].lyrics
+                                  })
+                                });
+                                const simText = await simResponse.json();
+                                console.log(simText);
+
+                                try {
+                                  const createSimResponse = await fetch(`${BASE_URL}/data/createSim`, {
+                                    method: "POST",
+                                    headers: {
+                                      "Content-Type": "application/json"
+                                    }, 
+                                    body: JSON.stringify({
+                                      "song1": song,
+                                      "artist1": artist,
+                                      "song2": odText[i].song,
+                                      "artist2": odText[i].artist,
+                                      "similarity": Math.round(simText.similarity * 100)
+                                    })
+                                  });
+                                  const createSimText = await createSimResponse.json();
+                                  console.log(createSimText);
+                                } catch (error) {
+                                  console.error("Error uploading sim data: ", error);
+                                }
+                              } catch (error) {
+                                console.error("Error creating similarity data for " + song + ", " + artist + " and " + odText[i].song + ", " + odText[i].artist + ": " + error);
+                              }
+                            }
+                          } catch (error) {
+                            console.error("Error fetching other data:", error);
+                          } 
+
+                          
+
+
                         }
                       } catch (err) {
                         console.log("Lyric Upload Error: ", err);
