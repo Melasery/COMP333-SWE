c19155e7f1c3b38a767d2d2c786511f0bf623c4b
felixberliner1 <61637800+felixberliner1@users.noreply.github.com>
2025-04-25 21:38:44 -0400

    Update DataModel.php

--- htdocs/Model/DataModel.php
+++ htdocs/Model/DataModel.php
@@ -24,6 +24,46 @@ class DataModel extends Database
         return $this->connection->insert_id;
     }
 
+    public function createSim($song1, $artist1, $song2, $artist2, $similarity)
+    {
+        $this->executeStatement(
+            "INSERT INTO lyricsimilarity (song1, artist1, song2, artist2, similarity) VALUES (?,?,?,?,?)",
+            [
+                ['type' => 's', 'value' => $song1],
+                ['type' => 's', 'value' => $artist1],
+                ['type' => 's', 'value' => $song2],
+                ['type' => 's', 'value' => $artist2],
+                ['type' => 'i', 'value' => $similarity]
+            ]
+        );
+        return $this-> connection->insert_id;
+    }
+
+    public function getSim($song1, $artist1, $song2, $artist2)
+    {
+        return $this->select(
+            "SELECT * FROm lyricsimilarity WHERE (song1 = ? AND artist1 = ? AND song2 = ? AND artist2 = ?) OR (song1 = ? AND artist1 = ? AND song2 = ? AND artist2 = ?)",
+            [
+                ['type' => 's', 'value' => $song1],
+                ['type' => 's', 'value' => $artist1],
+                ['type' => 's', 'value' => $song2],
+                ['type' => 's', 'value' => $artist2],                
+                ['type' => 's', 'value' => $song2],
+                ['type' => 's', 'value' => $artist2],
+                ['type' => 's', 'value' => $song1],
+                ['type' => 's', 'value' => $artist1],            
+            ]
+        );
+    }
+
+    public function getDataExcludingSongArtist($song, $artist)
+    {
+        return $this->select(
+            "SELECT * FROM songdata WHERE song != ? OR artist != ?",
+            [['type' => 's', 'value' => $song], ['type' => 's', 'value' => $artist]]
+        );
+    }
+
     public function getData($limit)
     {
         return $this->select(
